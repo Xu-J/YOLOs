8da4b630e257c5aa415b6ba83a4c417cd687e1b1
cured fish <51774024+Xu-J@users.noreply.github.com>
2020-10-27 10:08:15 +0800

    Update yolo1.py
    模型搭建完成

--- yolo1/yolo1.py
+++ yolo1/yolo1.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import numpy as np
+import torch
 
 class Yolo1net(nn.Module):
     def __init__(self):
@@ -26,11 +27,63 @@ class Yolo1net(nn.Module):
             nn.MaxPool2d(2, 2),
 
             nn.Conv2d(512, 256, 1, 1, 0),
-        nn.LeakyReLU(self.relu_alpha),
+            nn.LeakyReLU(self.relu_alpha),
             nn.Conv2d(256, 512, 3, 1, 1),
             nn.LeakyReLU(self.relu_alpha),
             nn.Conv2d(512, 256, 1, 1, 0),
-        nn.LeakyReLU(self.relu_alpha),
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.Conv2d(512, 256, 1, 1, 0)  ,# (b_s, 256, 28, 28)	rd=3
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(256, 512, 3, 1, 1) , # (b_s, 512, 28, 28)	rd=3
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(512, 256, 1, 1, 0) , # (b_s, 256, 28, 28)	rd=4
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(256, 512, 3, 1, 1)  ,# (b_s, 512, 28, 28)	rd=4
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(512, 512, 1, 1, 0) , # (b_s, 512, 28, 28)
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(512, 1024, 3, 1, 1) , # (b_s, 1024, 28, 28)
+            nn.LeakyReLU(self.relu_alpha),
+            nn.MaxPool2d(2, 2),
 
+            nn.Conv2d(1024, 512, 1, 1, 0) , # (b_s, 512, 14, 14)	rd=1
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(512, 1024, 3, 1, 1) , # (b_s, 1024, 14, 14)	rd=1
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(1024, 512, 1, 1, 0) , # (b_s, 512, 14, 14)	rd=2
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(512, 1024, 3, 1, 1)  ,# (b_s, 1024, 14, 14)	rd=2
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(1024, 1024, 1, 1, 0) , # (b_s, 1024, 14, 14)
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(1024, 1024, 3, 2, 1),
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(1024, 1024, 3, 1, 1),  # (b_s, 1024, 7, 7)
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Conv2d(1024, 1024, 3, 1, 1) , # (b_s, 1024, 7, 7)
+            nn.LeakyReLU(self.relu_alpha),
+        )
+
+        self.layer2 = nn.Sequential(
+
+            nn.Linear(1024 * 7 * 7, 4096),
+            nn.LeakyReLU(self.relu_alpha),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 30 * 7 * 7),
 
         )
+    def forward(self,input):
+        x = self.layer(input)
+        x = x.view(x.size(0),-1)
+
+        out = self.layer2(x)
+        out = out.view(-1,7,7,30)
+        return out
+
+
+
+yolo = Yolo1net()
+input = torch.rand(16,3,448,448)
+out = yolo(input)
+print(out.shape)
